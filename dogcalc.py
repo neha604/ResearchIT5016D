5c4baead92c4d5ebc41ee2bff4d2f8a074d249c1
Neha Singh <115962127+neha604@users.noreply.github.com>
2022-11-11 21:46:41 +1300

    Add files via upload

---
+++ dogcalc.py
@@ -0,0 +1,23 @@
+
+#simple python learning ,how to ask for user input and do calculations
+#this is just a simple game to find your dog's age in human years.
+
+
+
+#asking for dog's name from user
+dog_name=input("whats your dog name") 
+
+
+# asking for dog's age to calulate into human age
+dog_age=input("whats your dog age")
+
+human_age=int(dog_age)*7 #multipling by 7 will give dog's age in human years
+
+print(human_age) # printing our answer in numbers
+
+#printing the anwers in a sentence so its clear to be prescise.
+print("your dog",dog_name,"is",human_age,
+      "years old in human years")
+
+
+
